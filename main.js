659e12b55fdfcf8eaaac60ac6ac823f8a9377efd
schourode <jsr@malamute.dk>
2011-09-12 15:12:50 +0200

    Initial styling.

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
 var parser = /^(\S+)\s*-\s*(\S+)\s+([^(]+)\(([^)]+)\)$/,
-    persons = {},
     groups = {
-	'Barista': ['Christian','Jonathan','Luise','Morten','Uncas'],
-	'Guide': ['Magrethe','Malene','Maria'],
-	'P-vagt': ['Emil','Jørn','Rie'],
-	'Madhold': [],
-	'Alle': ['Barista','Guide','P-vagt','Madhold']
+        'Barista': ['Christian','Jonathan','Luise','Morten','Uncas'],
+        'Guide': ['Magrethe','Malene','Maria'],
+        'P-vagt': ['Emil','J\u00f8rn','Rie'],
+        'Madhold': ['Anne','Kristine'],
+        'Alle': ['Barista','Guide','P-vagt','Madhold']
+    },
+    colors = {
+        'Alle' : '#c00'
     };
 
-$.get('data/mandag.txt', function (data) {
-    var lines = data.split('\n');
+
+
+var table = $('#persons'),
+    thead = $('<thead>').appendTo(table),
+    headerRow = $('<tr>').appendTo(thead).append('<td>'),
+    tbody = $('<tbody>').appendTo(table);
+
+for (var i = 0; i < 32; i++) {
+    var decimal = i / 2 + 7,
+        hours = Math.floor(decimal),
+        minutes = decimal % 1 ? '30' : '00',
+        label = hours + ':' + minutes;
+        
+    $('<th>').appendTo(headerRow).text(label);
+}
+
+function loadData(hash) {
+    tbody.empty();
+    
+    $('h1 a').removeClass('selected')
+        .filter('[href="' + hash + '"]').addClass('selected');
+    
+    $.get('data/' + hash.substring(1) + '.txt', function (data) {
+        var persons = parseData(data);
+        renderTable(persons);
+    });
+}
+
+function parseData(data) {
+    var lines = data.split('\n'),
+        persons = {};
     
     for (var i in lines) {
         var tokens = lines[i].match(parser),
             entry = {
                 start: tokens[1].trim(),
                 end: tokens[2].trim(),
-                description: tokens[3].trim(),
+                text: tokens[3].trim(),
             },
             atendees = tokens[4].split(',');
         
@@ -35,5 +66,38 @@ $.get('data/mandag.txt', function (data) {
         }
     }
     
-    console.log(persons);
+    return persons;
+}
+
+function renderTable(persons) {
+    var names = Object.keys(persons).sort();
+    
+    for (var i in names) {
+        var name = names[i],
+            entries = persons[name],
+            row = $('<tr>').appendTo(tbody);
+        
+        $('<th>').text(name).appendTo(row);
+        
+        for (var j in entries) {
+            var entry = entries[j];
+            
+            $('<td colspan=4>').css('background', '#f3a').text(entry.text).appendTo(row);
+        }
+    }
+}
+
+$(window).bind('hashchange', function () {
+    loadData(location.hash);
 });
+
+if (location.hash) {
+    loadData(location.hash);
+}
+else {
+    var now = new Date(),
+        day = now.getDay(),
+        hash = $('h1 a').eq(day).attr('href');
+    
+    loadData(hash);
+}
